97e14cd9ea99d88c0d9c1f79725dd216de4f0712
salah eddin alshaal <salshaal@gmail.com>
2016-06-15 18:48:30 +0300

    initial commit - transition between two views

---
+++ UCLanTimetable/IndexViewController.swift
@@ -0,0 +1,18 @@
+//
+//  IndexViewController.swift
+//  UCLanTimetable
+//
+//  Created by Salah Eddin Alshaal on 15/06/16.
+//  Copyright © 2016 Salah Eddin Alshaal. All rights reserved.
+//
+
+import UIKit
+
+class IndexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
--- UCLanTimetable/ViewController.swift
+++ UCLanTimetable/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextBox: UITextField!
+    @IBOutlet weak var passTextBox: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        emailTextBox.keyboardType = UIKeyboardType.EmailAddress
+        //todo: check if internet is available
+        //todo: check if user is already logged in
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Actions
+    
+    @IBAction func loginButton_Clicked(sender: AnyObject) {
+        
+        var errorMsg: String? = nil
+        
+        if emailTextBox.text == "user@mail.com" && passTextBox.text == "pass" {
+            //success, move to main
+            self.performSegueWithIdentifier("loginSegue", sender: nil)
+        }
+        else if emailTextBox.text == ""{
+            errorMsg = "please enter an email"
+        }
+        else if !isValidEmail(emailTextBox.text!){
+            errorMsg = "please enter a valid email"
+        }
+        else if passTextBox.text == ""{
+            errorMsg = "please enter a password"
+        }
+        else{
+            errorMsg = "user name or password is not valid"
+        }
+        
+        if (errorMsg != nil) {
+            errorLabel.hidden = false
+            errorLabel.text = errorMsg!
+        }
+        else{
+            errorLabel.hidden = true
+        }
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        // print("validate calendar: \(testStr)")
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluateWithObject(testStr)
+    }
+    
 }
-
